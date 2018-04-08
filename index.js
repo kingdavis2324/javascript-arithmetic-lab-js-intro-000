2d146d6a1577713a4758e2d775925f5529860c2e
Kenny Davis <kjdavis2423@gmail.com>
2018-04-08 17:27:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ function subtract(a,b){
   var num = a - b;
   return num;
 }
+
+function multiply(a,b){
+  var num = a * b;
+  return num;
+}
+
+function divide(a, b){
+  var num = a / b;
+  return num;
+}
+
+function inc(n){
+  var num = n++;
+  return num;
+}
